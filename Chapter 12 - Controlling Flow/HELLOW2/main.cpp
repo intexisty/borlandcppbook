54181d359bd922f4643c53d7f69b191d25d522ed
Intexisty <legoguy253@gmail.com>
2020-05-31 18:47:46 -0500

    created HELLOW2

---
+++ Chapter 12 - Controlling Flow/HELLOW2/main.cpp
@@ -0,0 +1,12 @@
+// Prints "Hello World" on the screen.
+#include <iostream>
+
+// Define the PrintHelloWorld function
+void PrintHelloWorld() {
+    std::cout << "Hello World\n";
+}
+
+int main() {
+    PrintHelloWorld();
+    return 0;
+}
